c7aaf1d9146b3ae88ee100f821c8f57027d0299a
Vi Tran <contact.vitran@gmail.com>
2021-11-21 17:10:55 +1100

    Test 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+function submitData(nameString, emailString) {
+    let formData = {
+        name: nameString,
+        email: emailString
+    };
+    let postObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch('http://localhost:3000/users', postObj);
+}
